0ea53f9c15f63b4eee5a491c94c3a003fe70d191
Sreeginy <sreeginy98@gmail.com>
2024-01-25 21:36:51 +0530

    Update ToDo.java

--- app/src/main/java/com/sreeginy/tairp_todo/Model/ToDo.java
+++ app/src/main/java/com/sreeginy/tairp_todo/Model/ToDo.java
@@ -1,4 +1,31 @@
 package com.sreeginy.tairp_todo.Model;
 
 public class ToDo {
+
+    private String task;
+    private int id, status;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
